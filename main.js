6ed7900152c554c5603d9d0557ff7670d6243665
ShaneQin <664633146@qq.com>
2020-12-17 10:49:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -112,3 +112,18 @@ function create() {
   let result = Con.apply(obj, arguments);
   return result instanceof Object ? result : obj;
 }
+
+// 自己实现一个instanceof
+function myInstanceof(left, right) {
+  let prototype = right.prototype;
+  left = left.__proto__;
+  while (true) {
+    if (left === null || left === undefined) {
+      return false;
+    }
+    if (prototype === left) {
+      return true;
+    }
+    left = left.__proto__;
+  }
+}
